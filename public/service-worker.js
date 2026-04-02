cfeb3e0ac6dc86f640188fa48ab634b60856716f
Rakesh c <rc4496@srmist.edu.in>
2026-04-02 20:48:42 +0530

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "vaithiyar-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
